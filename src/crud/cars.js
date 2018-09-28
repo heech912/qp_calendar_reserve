fe2461804a1e4bff3713059ac520dc4264d7b8f3
heech912 <34178802+heech912@users.noreply.github.com>
2018-09-28 16:28:38 +0900

    proxy

--- src/crud/cars.js
+++ src/crud/cars.js
@@ -1,11 +1,21 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var mysql = require("mysql");
-var config = require("../config/db_config.json")
+var config = require("../../config")
+
+const options = {
+  user: config.get('MYSQL_USER'),
+  password: config.get('MYSQL_PASSWORD'),
+  database: 'zoocarSample'
+};
+
+if (config.get('INSTANCE_CONNECTION_NAME') && config.get('NODE_ENV') === 'production') {
+  options.socketPath = `/cloudsql/${config.get('INSTANCE_CONNECTION_NAME')}`;
+}
 
 var app = express();
 var router = express.Router();
-var con = mysql.createConnection(config);
+var con = mysql.createConnection(options);
 
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
